b0da6eaa761acead80ffca58fbb160c08efcf7d7
yamz-11 <wiratamajrz@gmail.com>
2022-03-07 15:20:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/home.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class home extends AppCompatActivity {
+
+    TextView Email, Password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        //
+        Email = findViewById(R.id.tf_nama);
+        Password = findViewById(R.id.tf_password);
+
+        Bundle bundle = getIntent().getExtras();
+
+        String email = bundle.getString("a");
+        String password = bundle.getString("b");
+
+        Email.setText(email);
+        Password.setText(password);
+    }
+
+}
+
